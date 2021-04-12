6b55766b1038b34440e9f35385b1b25a33e3dbc1
Tristan Peralta <tristanperalta@protonmail.com>
2021-04-12 14:02:52 +0800

    Observation top query

--- lib/covid19/covid_observation.ex
+++ lib/covid19/covid_observation.ex
@@ -1,6 +1,8 @@
 defmodule Covid19.CovidObservation do
   use Ecto.Schema
 
+  import Ecto.Query
+
   schema "covid_observations" do
     field :observation_date, :date
     field :country, :string
@@ -10,5 +12,25 @@ defmodule Covid19.CovidObservation do
     field :recovered, :integer
 
     timestamps()
+  end
+
+  def top(observation_date, max_results \\ nil) do
+    __MODULE__
+    |> where(observation_date: ^observation_date)
+    |> group_by([o], [o.observation_date, o.country])
+    |> select([o], %{
+      country: o.country,
+      confirmed: sum(o.confirmed),
+      recovered: sum(o.recovered),
+      deaths: sum(o.deaths)
+    })
+    |> order_by([o], desc: sum(o.confirmed))
+    |> max_results(max_results)
+  end
+
+  defp max_results(query, nil), do: query
+
+  defp max_results(query, count) do
+    limit(query, ^count)
   end
 end
